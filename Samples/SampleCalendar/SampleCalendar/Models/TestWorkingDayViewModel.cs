753f9ea0908d2f730d86c43b8909145584a954b9
Joao Silva <kappy@acydburne.com.pt>
2011-09-06 20:40:56 +0100

    Fixed poor man's persistence on holiday list. Created an holidayStrategy for the persisted list Added test form

---
+++ Samples/SampleCalendar/SampleCalendar/Models/TestWorkingDayViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleCalendar.Models {
+	public class TestWorkingDayViewModel {
+		public DateTime TestDate { get; set; }
+		public bool IsWorkingDay { get; set; }
+		public DateTime NextWorkingDay { get; set; }
+		public DateTime FifthWorkingDay { get; set; }
+	}
+}
---
+++ Samples/SampleCalendar/SampleCalendar/Services/ListHolidayStrategy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using DateTimeExtensions;
+using DateTimeExtensions.Strategies;
+using SampleCalendar.Models;
+
+namespace SampleCalendar.Services {
+	public class ListHolidayStrategy : IHolidayStrategy {
+		private IList<Holiday> holidayList;
+
+		public ListHolidayStrategy(IList<HolidayDTO> holidayList) {
+			this.holidayList = holidayList.Select(h => (Holiday)new FixedHoliday(h.Name, new DayInYear(h.Day.Month, h.Day.Day))).ToList();
+		}
+
+		public IEnumerable<Holiday> Holidays {
+			get { return holidayList; }
+		}
+
+		public bool IsHoliDay(DateTime day) {
+			foreach (var holiday in holidayList) {
+				if (holiday.IsInstanceOf(day)) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
